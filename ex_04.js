87f706f28bfc12086b271a7750aedcd493c7d04a
B3Y0NDER <dine.terzi@epitech.digital>
2024-09-30 11:41:21 +0200

    Update ex_04.js

--- ex_04.js
+++ ex_04.js
@@ -1,7 +1,7 @@
-console.log(typeof undefined);    
-console.log(typeof true);         
-console.log(typeof 42);          
-console.log(typeof 123n);         
-console.log(typeof Symbol());     
-console.log(typeof {});           
-console.log(typeof function(){}); 
+displayThisText(typeof undefined);    
+displayThisText(typeof true);         
+displayThisText(typeof 42);          
+displayThisText(typeof 123n);         
+displayThisText(typeof Symbol());     
+displayThisText(typeof {});           
+displayThisText(typeof function(){}); 
