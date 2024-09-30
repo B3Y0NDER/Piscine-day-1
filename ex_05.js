227de215c8b7342717347d4ce875706d9eaffee1
B3Y0NDER <dine.terzi@epitech.digital>
2024-09-30 13:28:32 +0200

    Update ex_05.js

--- ex_05.js
+++ ex_05.js
@@ -4,6 +4,6 @@ let cardcode = "1111 2222 3333 4444";
 let expiryDateMonth = 21;
 let expiryDateYear = 2042;
 let computedBankInfo = "John,Doe,1111 2222 3333 4444,21/2042";
-let computedBankInfos = firstname + lastname + cardcode + expiryDateMonth + expiryDateYear;
+let computedBankInfos = firstname +"," lastname +"," cardcode +"," expiryDateMonth +"/" expiryDateYear;
 
 function displayInCard(computedBankInfos) {displayInCard(computedBankInfos);}
