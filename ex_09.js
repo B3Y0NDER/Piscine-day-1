ae1f528b6e94d65acc61156e450c6df9e8ba53f1
B3Y0NDER <dine.terzi@epitech.digital>
2024-09-30 14:52:22 +0200

    Update ex_09.js

--- ex_09.js
+++ ex_09.js
@@ -1,1 +1,17 @@
+let type;
+if (typeof input_var === "string") {
+    type = "String";
+} else if (typeof input_var === "boolean") {
+    type = "Boolean";
+} else if (Number.isInteger(input_var)) {
+    type = "Integer";
+} else if (typeof input_var === "number") {
+    type = "Float";
+}
 
+displayThisText(`La valeur de la variable est '${input_var}'.`);
+displayThisText(`Son type est '${type}'.`);
+
+if (input_var === 42 || input_var === "42") {
+    displayThisText("C'est le sens de la vie.");
+}
