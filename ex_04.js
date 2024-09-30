3e1d69070cc533c1ce8870d665d146df11c94f11
B3Y0NDER <dine.terzi@epitech.digital>
2024-09-30 10:59:41 +0200

    Create ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,7 @@
+console.log(typeof undefined);    
+console.log(typeof true);         
+console.log(typeof 42);          
+console.log(typeof 123n);         
+console.log(typeof Symbol());     
+console.log(typeof {});           
+console.log(typeof function(){}); 
